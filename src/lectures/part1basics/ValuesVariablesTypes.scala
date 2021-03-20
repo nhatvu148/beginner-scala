cddceaaeed2b4a334bad4369f3e054708389f74a
nhatvu148 <nhatvu148@gmail.com>
2021-03-21 00:14:47 +0900

    initial commit

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,12 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x = 42
+  println(x)
+  var result = 1.+(2)
+  println(result)
+
+  val yy : Int = 100 // Int is a class! yy is object, 100 is object, + is function, No primitives in Scala
+
+  val aString = "Hello string!"
+}
