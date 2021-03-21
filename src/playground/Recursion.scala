1146a8b81627b250e762e8a3d2a7db08435b0d63
nhatvu148 <nhatvu148@gmail.com>
2021-03-22 01:12:53 +0900

    Recursion

---
+++ src/playground/Recursion.scala
@@ -0,0 +1,61 @@
+package playground
+
+import scala.annotation.tailrec
+
+object Recursion extends App {
+  def factorial(n: Int): Int =
+    if (n < 1) 1
+    else {
+      println("Computing factorial of " + n)
+      val result = n * factorial(n - 1)
+      println("Computed factorial of " + n)
+      result
+    }
+
+  //  println(factorial(5000))
+
+  def anotherFactorial(n: Int): BigInt = {
+    @tailrec
+    def factHelper(x: Int, accumulator: BigInt): BigInt =
+      if (x < 1) accumulator
+      else factHelper(x - 1, x * accumulator) // TAIL RECURSION = use recursive call as the last expression
+
+    factHelper(n, 1)
+  }
+
+  println(anotherFactorial(1000))
+
+  // WHEN YOU NEED LOOPS, USE TAIL RECURSION
+
+  @tailrec
+  def concatenateTailrec(aString: String, n: Int, accumulator: String): String =
+    if (n <= 0) accumulator
+    else concatenateTailrec(aString, n - 1, aString + accumulator)
+
+  println(concatenateTailrec("hello", 3, ""))
+
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailrec(t: Int, isStillPrime: Boolean): Boolean =
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailrec(t - 1, n % t != 0 && isStillPrime)
+
+      isPrimeTailrec(n / 2, isStillPrime = true)
+  }
+
+  println(isPrime(2003))
+  println(isPrime(629))
+
+  def fibonacci(n: Int): Int = {
+    @tailrec
+    def fiboTailrec(i: Int, last: Int, nextToLast: Int): Int =
+      if (i >= n) last
+      else fiboTailrec(i + 1, last + nextToLast, last)
+
+    if (n < 2) 1
+    else fiboTailrec(2, 1, 1)
+  }
+
+  println(fibonacci(8))
+}
